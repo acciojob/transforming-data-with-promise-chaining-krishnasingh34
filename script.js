a751855268ea25760f557e2f041087903d84c6d4
Krishna Singh <144099384+krishnasingh34@users.noreply.github.com>
2025-03-01 15:06:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your JS code here. If required.
+ let textbox = document.querySelector('#ip')
+let output = document.querySelector('#output')
+let btn = document.querySelector('#btn')
+btn.addEventListener('click',(e)=>{
+	e.preventDefault()
+	let text = textbox.value
+	function updateTask(task, time){
+        return new Promise((resolve, reject)=>{
+            setTimeout(()=>{
+                resolve(task)
+            },time)
+        })
+    }
+    updateTask(text, 2000)
+    .then(data =>{
+        output.innerText = 'Result: '+data
+        return updateTask(data*2, 2000)
+    })
+    .then(data =>{
+        output.innerText = 'Result: '+data
+        return updateTask(data-3, 1000)
+    })
+    .then(data =>{
+        output.innerText = 'Result: '+data
+        return updateTask(data/2, 1000)
+    })
+    .then(data =>{
+        output.innerText = 'Result: '+data
+        return updateTask(data+10, 1000)
+    })
+    .then(data =>{
+        output.innerText = 'Result: '+data
+    })
+    .catch(err => {console.log(err)})
+})
